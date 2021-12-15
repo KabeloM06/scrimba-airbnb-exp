31134f7d0c302831ac8cf1d315fc90897e5139af
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2021-12-15 10:31:30 +0200

    fixed css that was not linked correcly

--- src/App.js
+++ src/App.js
@@ -2,32 +2,28 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
+import data from "./data"
+import "./style.css"
 
 export default function App() {
             // <Hero />
+
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
     return (
         <div>
             <Navbar />
-            <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                location="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            {cards}
         </div>
     )
 }
